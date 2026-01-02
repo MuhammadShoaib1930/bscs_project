20d9e4ba510e26c217b047345c4586e68bd3da88
Muhammad shoaib <shoaibsn54321@gmail.com>
2026-01-02 11:36:55 +0500

    updatetd buges and finished project

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
